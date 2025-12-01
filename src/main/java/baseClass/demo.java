32b775988ea775a24bebf48f76f0cdf262841832
naveenprasadr2627 <naveenprasad2627@gmail.com>
2025-12-01 15:44:23 +0530

    Updated code

---
+++ src/main/java/baseClass/demo.java
@@ -0,0 +1,11 @@
+package baseClass;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Base class is working");
+
+	}
+
+}
